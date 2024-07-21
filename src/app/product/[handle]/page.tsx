0ef010c03880fbef0158d2fee9a31729bb8c5080
Bryan Alvarado <bryan.alvarado@wunderdog.fi>
2024-07-21 17:35:46 +0200

    add seo metadata

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductView } from "app/components/home/product/ProductView";
+import { ProductView } from "app/components/product/ProductView";
 import { getProducts } from "app/services/shopify/products";
 import { redirect } from "next/navigation";
 
@@ -8,13 +8,29 @@ interface ProductPageProps {
   };
 }
 
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  const id = searchParams.id;
+  const products = await getProducts(id);
+  const product = products[0];
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images: [product.image],
+    },
+  };
+}
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id;
   const products = await getProducts(id);
   const product = products[0];
+
   if (!id) {
-    redirect("/store");
+    redirect("/");
   }
 
-  return <ProductView product={product}></ProductView>;
+  return <ProductView product={product} />;
 }
